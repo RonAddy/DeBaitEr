832ca5b13e435936e08a5f74c622846cd5f18d2e
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-30 14:54:03 -0400

    "Added middleware checkers in server.js for morgan, body-parser, cookie-parser, session, and passport...Specified view engine type and where app will access static files"

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//require express package
 const express = require('express');
+
+//require other dependecies
 const logger = require('morgan');
 const path = require('path');
 const bodyParser =require('body-parser');
@@ -7,28 +10,45 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-require('dotenv').config();
-
+//initializeng my app
 const app = express();
 
+//adding .env file and contents
+require('dotenv').config();
+
+//sets the port manually to 3000 or from an environmental variable
 const PORT = process.env.PORT || 3000;
 
+//set app to listen on this port
 app.listen(PORT, () => {
   console.log(`🗣 (${PORT})‼️  👥👥👥👤👥👥💭`)
 })
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+//setting up the middlewares: logger, auth, bodyParser, etc.
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+//app refers to public folder for static files
+app.use(express.static(path.join(__dirname, 'public')));
 
+//app will use ejs view engine, within views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+//app will render index.ejs file after receiving a get request to the root path
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 
 
