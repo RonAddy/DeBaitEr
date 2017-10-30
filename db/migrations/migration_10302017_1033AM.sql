68305b0f748ddd08d28bef5f7e488a698e0f99af
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-30 12:11:58 -0400

    "Created db and migrations directories...Filled in first migration file (10:33AM) to create users and bait tables"

---
+++ db/migrations/migration_10302017_1033AM.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS bait;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  email VARCHAR NOT NULL,
+  fname VARCHAR(255),
+  lname VARCHAR(255)
+);
+
+CREATE TABLE bait (
+  id SERIAL PRIMARY KEY,
+  category VARCHAR(255) UNIQUE NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  author VARCHAR(255) UNIQUE NOT NULL REFERENCES user(username)
+);
