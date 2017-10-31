6ff12b89a7b1ab521c4d6f98f4535a01cc7b054a
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-31 09:34:12 -0400

    "Created database queries for users in the Users.js model file"

---
+++ models/Users.js
@@ -0,0 +1,20 @@
+//requore
+const db = require('../db/config');
+
+const User = {};
+
+User.findByUserName = userName => {
+  return db.oneOrNone(`
+    SELECT * FROM users
+    WHERE username = $1`,[userName]);
+};
+
+User.create = user => {
+  return db.one(`
+    INSERT INTO users
+    (username, password_digest, email, fname, lname)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *`, [user.username, user.password_digest, user.email, user.fname, user.lname]);
+};
+
+module.exports = User;
