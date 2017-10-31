44dc217c7ae46c062162380c77a01bd32db5e369
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-31 10:27:44 -0400

    "Created routes for user login and register pages..working on controller"

---
+++ routes/auth-routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+//handles request to the login path, renders login page
+authRouter.get('/login', authHelpers.loginRedirect, (req, res, next) => {
+  res.render('auth/login');
+});
+
+//handles requests to the register path, renders user register page
+authRouter.get('/register', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/register');
+});
+
+//handles user register form, will create user and input data to databse via controller, then model
+authRouter.post('/register', usersController.create);
