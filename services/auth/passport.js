d921ecd29f2299880248b3ea6fd666d65b19f622
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-31 09:57:36 -0400

    "Created all neccessary functions to authorize users in services directory files...working on routes"

---
+++ services/auth/passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+
+const User = require('../../models/Users')
+
+
+function initPassport () => {
+  passport.serializeUser((user, done)) => {
+    done(null, user.username);
+  });
+
+  passport.deserializeUser((username,  done) => {
+    User.findByUserName(username)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
+    });
+  });
+};
+
+module.exports = initPassport();
