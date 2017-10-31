4dd04893f3ef66ec9d9ab3e8b388fdd64d4b3e12
Aaron Bart-Addison <abartaddison12@gmail.com>
2017-10-31 10:39:49 -0400

    "Created controller action for creating a new user account."

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,3 +2,27 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user.js');
 
 const usersController = {};
+
+usersController.create = (req, res) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    password_digest: hash,
+    email: req.body.email,
+    fname: req.body.fname,
+    lname: req.body.lname,
+  })
+  .then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+        res.redirect('/user');
+    });
+  })
+  .catch( err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+};
+
+module.exports = usersController;
